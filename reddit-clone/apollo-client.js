626ddd9b1ad4e60a2953202f09b4c8e8869a6f94
ntaztonny <atozpp@yahoo.com>
2022-07-05 09:13:19 +0200

    added an apollo client and stepzen client congfig

---
+++ reddit-clone/apollo-client.js
@@ -0,0 +1,16 @@
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://publica2eb0f85d704f3a0.stepzen.net/api/stepzen_API/__graphql",
+  headers: {
+    Authorization: `Apikey ${process.env.NEXT_PUBLIC_STEPZEN_KEY}`,
+  },
+  cache: new InMemoryCache(),
+});
+
+export default client;
